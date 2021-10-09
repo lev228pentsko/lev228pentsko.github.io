58e997c9a8694219a8a5b38425794fc66ae62cdd
lev228pentsko <61650411+lev228pentsko@users.noreply.github.com>
2021-10-09 10:42:11 +0300

    Add files via upload

---
+++ програмування в дома/firebase/index.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBiwynKOYXKva2AHrJncLq7dOoKhk6_oOM",
+    authDomain: "sl-lev.firebaseapp.com",
+    projectId: "sl-lev",
+    storageBucket: "sl-lev.appspot.com",
+    messagingSenderId: "534991083572",
+    appId: "1:534991083572:web:1a9a628cb58382f35bd261",
+    measurementId: "G-SMPQRY3L0M"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+  var db = firebase.firestore();
+     
