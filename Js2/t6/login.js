1fa553a287e69c5d622c4b1b33a4117e51329157
Pentsko Lev <pentskolev197@gmail.com>
2021-11-06 15:24:45 +0200

    add t6

--- Js2/t6/login.js
+++ Js2/t6/login.js
@@ -2,7 +2,29 @@ function login(){
     const email = document.getElementById('email');
     const password = document.getElementById('pass');
 
-    db.collection('users').get().where("email", "==", email.value).then( (res)=>{
-        console.log(res.doc())
+    db.collection('users').where("email", "==", email.value).get().then( (res)=>{
+       res.forEach(function(doc) {
+            let user = {
+                id: doc.id,
+                ...doc.data()
+            }
+            console.log(user.password);
+            console.log(password.value);
+            if(user.password == password.value){
+                localStorage.setItem("user", JSON.stringify(user));
+                window.location.href = "index.html";
+            }else{
+                alert("Невірний пароль!")
+            }
+            console.log(user);
+           
+        });
     });
 }
+function checkUser(){
+    const user = localStorage.getItem("user");
+    if(user != null || user != ""){
+        window.location.href = "index.html";
+    }
+}
+checkUser();
--- Js2/t6/main.js
+++ Js2/t6/main.js
@@ -4,8 +4,46 @@ function isLoged(){
         window.location.href = 'login.html'
     }
 
-
     console.log(user);
 }
 isLoged();
 
+let articles = [];
+
+function getArticles(){
+    db.collection("articles")
+    .get()
+    .then( res => {
+        res.forEach( doc => {
+            let article ={
+                id: doc.id,
+                ...doc.data()
+            }
+            articles.push(article);
+            drawArticles(article.id, article.title);
+        })
+    })
+}
+getArticles()
+
+function drawArticles(id, title){
+    const box = document.createElement('div');
+    const boxImage = document.createElement('div');
+    const boxTitle = document.createElement('div');
+
+    box.classList.add('box');
+    box.classList.add('d-flex');
+    box.classList.add('mb-3')
+    box.id = id;
+
+    boxImage.classList.add('box-image');
+
+    boxTitle.classList.add('box-title');
+
+    boxTitle.innerText = title;
+
+    box.appendChild(boxImage);
+    box.appendChild(boxTitle);
+
+    document.getElementById('articles-box').appendChild(box);
+}
