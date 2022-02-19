3e3f4398d377a5045020285522ba294e210ab6ab
lev228pentsko <61650411+lev228pentsko@users.noreply.github.com>
2022-02-19 15:06:41 +0200

    Add files via upload

--- t14/main.js
+++ t14/main.js
@@ -1,154 +1,82 @@
-const users = [
-	{
-		name: 'Osvaldo',
-		lastname: 'Mccants',
-		email: 'svaldomcantos@gmail.com',
-		gender: 'male',
-		confirmed: true
-	},
-	{
-		name: 'Maximina',
-		lastname: 'Lasso',
-		email: 'maximina2002@bigmir.net',
-		gender: 'female',
-		confirmed: false
-	},
-	{
-		name: 'Sandee',
-		lastname: 'Snelson',
-		email: 'sandeesandee@yahoo.com',
-		gender: 'female',
-		confirmed: true
-	},
-	{
-		name: 'Everette',
-		lastname: 'Epperson',
-		email: 'epperson24@gmail.com',
-		gender: 'male',
-		confirmed: true
-	},
-	{
-		name: 'Dante',
-		lastname: 'Duchesne',
-		email: 'danteduch@yahoo.com',
-		gender: 'male',
-		confirmed: false 
-	},
-	{
-		name: 'Jenise' ,
-		lastname: 'Jones',
-		email: 'janiejo@gmail.com',
-		gender: 'female',
-		confirmed: true
-	},
-	{
-		name: 'Duncan',
-		lastname: 'Detwiler',
-		email: 'detwillerd@bigmir.net',
-		gender: 'male',
-		confirmed: true
-	},
-	{
-		name: 'Luna',
-		lastname: 'Logan',
-		email: 'lunalogan@yahoo.com',
-		gender: 'female',
-		confirmed: false
-	},
-	{
-		name: 'Verlie',
-		lastname: 'Voigt',
-		email: 'verlie007@gmail.com',
-		gender: 'female',
-		confirmed: true
-	},
-	{
-		name: 'Theron',
-		lastname: 'Tisby',
-		email: 'tisbyteo@gmail.com',
-		gender: 'male',
-		confirmed: false 
-	},
-	{
-		name: 'Sacha',
-		lastname: 'Sherron',
-		email: 'sachaato@gmail.com',
-		gender: 'female',
-		confirmed: true
-	},
-	{
-		name: 'Dani',
-		lastname: 'Dehoyos',
-		email: 'danid@bigmir.net',
-		gender: 'female',
-		confirmed: true
-	},
-	{
-		name: 'Lu',
-		lastname: 'Laine',
-		email: 'lulaine@yahoo.com',
-		gender: 'female',
-		confirmed: false
-	},
-	{
-		name: 'Carlos',
-		lastname: 'Conkle',
-		email: 'carlos228@bigmir.net',
-		gender: 'male',
-		confirmed: true
-	},
-	{
-		name: 'Lucio',
-		lastname: 'Lobel',
-		email: 'luciol@gmail.com',
-		gender: 'male',
-		confirmed: true
-	},
-	{
-		name: 'Tanya',
-		lastname: 'Huston',
-		email: 'taniahus@bigmir.net',
-		gender: 'female',
-		confirmed: false 
-	},
-	{
-		name: 'Daniel',
-		lastname: 'Coto',
-		email: 'dannyc@yahoo.com',
-		gender: 'male',
-		confirmed: true 
-	}
-];
+document.addEventListener('DOMContentLoaded', async function(){   
 
-const users_table = document.getElementById('users_table');
-
-let users_html = ``;
-
-for(i = 0; i < users.length; i++ ){
-
-    let image = `male.svg`;
-    if(users[i].gender == 'female'){image = 'female.svg'}
-
-    let box = ``;
-    if(users[i].confirmed == true){box = `checked`}
-
-    users_html += 
-    `
-<tr>
-    <td>${i + 1}</td>
+    let home = await axios.get("templates/home.html");
+    let notfound = await axios.get("templates/404.html");   
+    let orders = await axios.get("templates/orders.html");
+    let products = await axios.get("templates/products.html");
     
-    <td>${users[i].name} ${users[i].lastname}</td>
-    
-    <td class="centered"> <img src=${image} class="icon"> </td>
-    
-    <td>${users[i].email}</td>
-    
-    <td class="centered"> <input type="checkbox" ${box}> </td>
-</tr>
-    `
 
-}
+    const data =  {
+        message: 'Hello Vue.js!',
+        currentPath: window.location.hash,
+        orders: [],
+        products: []
+    };
 
-users_table.innerHTML = users_html
+    const Home = {
+        template: home.data,
+    };
 
-console.log(users)
+    const NotFound = {
+        template: notfound.data
+    };
+
+    const Orders = {
+        template: orders.data,
+        props: ['orders']
+    };
+
+    const Products = {
+        template: products.data,
+        props: ['products']
+    };
+
+    const routes = {
+        '/': Home,
+        '/home': Home,
+        '/products': Products,
+        '/orders': Orders,
+        '/not-found': NotFound,
+
+    }
+
+    const app = {
+        data() {  return data },
+        methods: { 
+            getOrders(){
+                db.collection('orders')
+                .get()
+                .then( res => {
+                    res.forEach( element =>{
+                        console.log(element);
+                        this.orders.push(element.data());
+                    });
+                } )
+            },
+            getProducts(){
+                db.collection('products')
+                .get()
+                .then( res => {
+                    res.forEach( element =>{
+                        console.log(element);
+                        this.products.push(element.data());
+                    });
+                } )
+            }
+        },
+        components: { },
+        computed: {
+            currentView() {
+                return routes[this.currentPath.slice(1) || '/'] || NotFound
+            }
+        },
+        mounted() {
+            window.addEventListener('hashchange', () => {
+                this.currentPath = window.location.hash
+            })
+            this. getOrders();
+            this. getProducts();
+        }
+    }
+    Vue.createApp(app).mount('#app');
+})
